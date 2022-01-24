bd2a79c9e752207f5f47d8c5ad1b5f6d4f4da8ff
Seamus McShane <seamus.mcshane@outlook.com>
2022-01-24 21:18:48 +0000

    Add Rest API app to return sensor info via json.
    	- Currently hardcoded to use the EnviroPlus.

---
+++ SensorMon.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python3
+import threading
+import atexit
+
+# The board in use
+from boards.enviroplus import EnviroPlus
+enviroPlus = EnviroPlus();
+
+# Threading
+updateLock = threading.Lock()
+updateThread = threading.Thread()
+
+## Rest API
+import flask
+from flask import Flask,request, jsonify, Response
+
+def createRestApp():
+	app = Flask(__name__)
+
+	def interrupt():
+		global updateThread
+		updateThread.cancel()
+
+	def update():
+		global updateThread
+		with updateLock:
+			enviroPlus.updateValues()
+
+		updateThread = threading.Timer(1, update, ())
+		updateThread.start()
+
+	def startUpdateThread():
+
+		# Preinit
+		global updateThread
+
+		# Update Thread
+		updateThread = threading.Timer(1, update, ())
+
+		# Thread Started
+		updateThread.start()
+
+	# Default path
+	@app.route('/', methods=['GET'])
+	def home():
+		# A simple redirect to the values page
+		return '''<meta http-equiv=\"refresh\" content=\"time=0; URL=/values" \/>'''
+
+	# Our Values Path
+	@app.route('/values', methods=['GET'])
+	def api_all():
+		with updateLock:
+
+			# Response is the sensor values wrapped in json
+			return Response(response=enviroPlus.getJSONValues(), status=200, mimetype="application/json")
+
+	startUpdateThread()
+
+	app.run(host="0.0.0.0", port="8080")
+
+	# Ctrl-C handling
+	atexit.register(interrupt)
+
+	return app
+
+# create the rest app
+app = createRestApp()
+
+# Debug
+# from time import sleep
+#while True:
+#	enviroPlus.updateValues()
+#	sleep(1)

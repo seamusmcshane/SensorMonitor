024670eec8b51ca13f9d0658c080f17c32fe1886
Seamus McShane <seamus.mcshane@outlook.com>
2022-02-22 15:20:04 +0000

    EnviroPlus support redone to make it consistent with the WaveshareESH support.
    	- Smooth the values using the CBuffer datastructure.
    	- Use the PICPU object for CPU temperature handling.
    	- Fix left over class variables from initial development.
    	- Remove uneeded math import.
    	- Fix a few typos.

--- boards/enviroplus.py
+++ boards/enviroplus.py
@@ -13,47 +13,40 @@
 # See the GNU Lesser General Public License Version 3 for more details.
 #
 
-# Enviroplus Board support
+# EnviroPlus Board support
 # Devices supported		-	Temperature, Humidity and Pressure, Lux, Proximity, Gas and LCD.
 # Devices not supported	-	Noise and Particulate matter addon.
 
-# Temperature, Humidity and Pressure
 from smbus import SMBus
+I2C_DEV=SMBus(1)
+
+# Temperature, Humidity and Pressure
 from sensors.BME280 import BME280
+
+# CPU Temp
 from subprocess import PIPE, Popen
 
 # The Lux/Proximity sensors
 from ltr559 import LTR559
 
-
 # Gas sensor
 from sensors import MICS6814
-
-# math
-import math
 
 # EnviroPlus OLED Display
 from boards.enviroplusdisplay import Display
 
+from utility.cbuffer import CBuffer
+from utility.picputemperature import PICPUTemp
 
 # Assuming updating at 1 sample per second this is ten seconds of samples
-SAMPLE_WINDOW_LEN = 10;
+SAMPLE_WINDOW_LEN = 10
 
-
-# A class to describe what our json returned values will look like
+# A class to describe what our JSON returned values will look like
 import json
 class Values:
 
-	proximity = 0.0
-	lux = 0.0
-	temperature = 0.0
-	humidity = 0.0
-	pressure = 0.0
-	reducing = 0.0
-	oxidising = 0.0
-	nh3 = 0.0
-
 	def __init__(self):
+
 		self.proximity = 0.0
 		self.lux = 0.0
 		self.temperature = 0.0
@@ -69,52 +62,6 @@ class Values:
 # The board class
 class EnviroPlus:
 
-	# frame count, we start on 1 as this is the first update
-	frame=1;
-
-	# startup display mode at startup
-	lcd_mode=LCD_MODE.SENSORS;
-
-	# LCD
-	lcd=0;
-	fb=0;
-
-	# THS
-	bme280=0;
-
-	bme280_raw_temps = [];						# to account for noise influence on sensor readings
-	bme280_raw_humidity_window = [];			# to account for noise influence on sensor readings
-	bme280_raw_pressure_window  = [];			# to account for noise influence on sensor readings
-
-	cpu_temps = [];								# to account for cpu temp influence on sensor readings
-
-	smooth_size = SAMPLE_WINDOW_LEN;			# Depends on how often you sample. Compensation against a PI CPU burst heating up the sensor.
-
-	# This you will need to calibrate this as they depend on the PI. Best done with a physical thermometer or a calibrated reference sensor.
-	smooth_factor = 0.9;						# Depends on how close the sensor is to the PI, and the Pi's thermals. Here there is a tall header so 1cm distance and pi is inside a aluminum case, with very low load.
-
-	# LUX
-	ltr559=0;
-
-	ltr559_avg_window = SAMPLE_WINDOW_LEN		# size of the avg window
-	ltr559_raw_proximity_window = [];			# to account for noise influence on sensor readings
-	ltr559_raw_lux_window = [];					# to account for noise influence on sensor readings
-
-
-	# Gas
-	mics6814=0;
-
-	mics6814_avg_window = SAMPLE_WINDOW_LEN		# size of the avg window
-	mics6814_raw_reducing_window = [];			# to account for noise influence on sensor readings
-	mics6814_raw_oxidising_window = [];			# to account for noise influence on sensor readings
-	mics6814_raw_nh3_window = [];				# to account for noise influence on sensor readings
-
-	# Particulate mater addon need HW to test
-	# pms5003=0;
-
-	# Sensor values for formating into json
-	currentValues = Values()
-
 	# Setup the LCD controller and backing frame buffer
 	def initDisplay(self):
 
@@ -125,9 +72,16 @@ class EnviroPlus:
 	# Setup the BME Temperature, Humidity and Pressure sensor
 	def initBME280(self):
 
-		# Create a BME280 instance (SMBus 1)
-		self.bme280 = BME280(i2c_dev=SMBus(1))
+		# CPU Temp
+		self.cpu_temp = PICPUTemp(SAMPLE_WINDOW_LEN)
 
+		# Buffers for BME280 stats
+		self.bme280_temps = CBuffer(SAMPLE_WINDOW_LEN)
+		self.bme280_humidity = CBuffer(SAMPLE_WINDOW_LEN)
+		self.bme280_pressure = CBuffer(SAMPLE_WINDOW_LEN)
+
+		# Create a BME280 instance (SMBus 1)
+		self.bme280 = BME280(i2c_dev=I2C_DEV)
 		self.bme280.setup(mode="forced", temperature_oversampling=16, pressure_oversampling=16)
 
 		print("BME280 Ready")
@@ -135,12 +89,21 @@ class EnviroPlus:
 	# Setup the LTR559 Proximity and Light Sensor
 	def initLTR559(self):
 
+		# Buffers for the LTR559 Stats
+		self.ltr559_lux = CBuffer(SAMPLE_WINDOW_LEN)
+		self.ltr559_prox = CBuffer(SAMPLE_WINDOW_LEN)
+
 		self.ltr559 = LTR559()
 
 		print("LTR559 Ready")
 
 	# Setup the MICS6814 Analog Gas Sensor
 	def initMICS6814(self):
+
+		# Buffers for the MICS6814 Stats
+		self.mics6814_reducing = CBuffer(SAMPLE_WINDOW_LEN)
+		self.mics6814_oxidising = CBuffer(SAMPLE_WINDOW_LEN)
+		self.mics6814_nh3 = CBuffer(SAMPLE_WINDOW_LEN)
 
 		self.mics6814 = MICS6814
 
@@ -159,15 +122,43 @@ class EnviroPlus:
 
 		print("MICS6814 Ready")
 
-	# Initialises all the sub compoenents when an EnviroPlus object is created.
-	def __init__(self):
+	# Initialises all the sub components when an EnviroPlus object is created.
+	def __init__(self, smooth_factor = 0.9):
+
+		# You will need to calibrate this.
+		# Best done with a physical thermometer or a calibrated reference sensor.
+		# Value depends on how close the sensor is to the PI, and the Pi's thermals.
+		# 0.9 was used with a tall header outside a case with - 1cm distance to the PI.
+		# PI is inside a aluminium case, with very low load.
+		self.smooth_factor = smooth_factor
+
+		# Sensor values for formatting into JSON
+		self.currentValues = Values()
 
 		self.initBME280()
 		self.initLTR559()
-		self.initMICS6814();
+		self.initMICS6814()
 		self.initDisplay()
 
-		print("EnviroPlus Ready");
+		print("EnviroPlus Ready")
+
+	def smooth_temp_value(self, raw_value, smooth_factor):
+		""" Adjusts the BME280 temperature reading to account for distance
+		to the PI CPU which genrates enough heat to affect the reading.
+		Smoothing assumes you are sampling the sensor more than you are
+		storing the data.
+		"""
+
+		self.bme280_temps.addValue(raw_value)
+
+		cpu_avg_temp = self.cpu_temp.getTemperature()
+
+		avg_temp = self.bme280_temps.getValue()
+
+		# Dampens the average temp by the cpu average temp.
+		# Dampening is scaled using a smooth factor for fine tuning.
+		# Adjust smooth_factor based on how close the sensor is to the cpu/case and how hot they get.
+		return (avg_temp - ((cpu_avg_temp - avg_temp) / smooth_factor))
 
 	# Fetches the current cpu temperature
 	def get_cpu_temperature(self):
@@ -177,90 +168,56 @@ class EnviroPlus:
 		#print ("output 1: ", output.strip())
 		return float(output) * 0.001
 
-	# This stores the raw value in a window and returns the current average of the window. Window limited to window len.
-	def smooth_value_generic(self, raw_window, window_len, raw_value):
-
-		# Add our new value to the end
-		raw_window.append(raw_value)
-
-		# If the length is greater than the max window size, remove the first sample
-		if (len(raw_window) > window_len):
-			del raw_window[0]
-
-		# Return the new average of all samples in the window
-		return sum(raw_window) / float(len(raw_window))
-
-	# Assumes you are sampling the sensor more than you are storing the data
-	def smooth_temp_value(self, window_len, raw_value):
-
-		# Our raw cpu temp window
-		ncpu_temp = self.get_cpu_temperature()
-		self.cpu_temps.append(ncpu_temp)
-
-		if (len(self.cpu_temps) > self.smooth_size):
-			del self.cpu_temps[0]
-
-		# Our raw temp window
-		self.bme280_raw_temps.append(raw_value)
-
-		if (len(self.bme280_raw_temps) > window_len):
-			del self.bme280_raw_temps[0]
-
-		# Our raw average temperator window average
-		avg_temp = sum(self.bme280_raw_temps) / float(len(self.bme280_raw_temps))
-
-		# Our average cpu temp window
-		smoothed_cpu_temp = sum(self.cpu_temps) / float(len(self.cpu_temps))
-
-		# Dampens the average temp by the cpu average temp.
-		# Dampening is scaled using a smooth factor for fine tuning.
-		# Adjust smooth_factor based on how close the sensor is to the cpu/case and how hot they get.
-		# see smooth_factor at the top of this class.
-		return (avg_temp - ((smoothed_cpu_temp - avg_temp) / self.smooth_factor))
-
-
-
-
 	def updateValues(self):
+		""" Performs a collection of values from supported devices """
 
+		# Update the cpu temp which is used to smooth/adjust the bme280 temp
+		self.cpu_temp.update()
 
-		# ltr559
-		proximity = self.ltr559.get_proximity()
-
-		lux = self.ltr559.get_lux();
-		lux = self.smooth_value_generic(self.ltr559_raw_lux_window, SAMPLE_WINDOW_LEN, lux)
-
-		# BME280 lib is modified to coalesc the three calls
+		# BME280 lib is modified to coalesce the three calls
 		thp = self.bme280.get_thp()
-		temperature =  self.smooth_temp_value(SAMPLE_WINDOW_LEN, thp[0])	# calls temperature specifc smooth
+		temperature =  self.smooth_temp_value(thp[0], self.smooth_factor)	# calls temperature specific smooth
+		self.bme280_humidity.addValue(thp[1])
+		humidity = self.bme280_humidity.getValue()
+		self.bme280_pressure.addValue(thp[2])
+		pressure = self.bme280_pressure.getValue()
+
+		# Write current smoothed data to JSON values
+		self.currentValues.temperature, self.currentValues.humidity, self.currentValues.pressure = temperature, humidity, pressure
+
 		# LTR559
 		r_proximity = self.ltr559.get_proximity()
 		r_lux = self.ltr559.get_lux()
 
-		humidity = self.smooth_value_generic(self.bme280_raw_humidity_window, SAMPLE_WINDOW_LEN, thp[1])
+		self.ltr559_prox.addValue(r_proximity)
+		proximity = self.ltr559_prox.getValue()
+		self.ltr559_lux.addValue(r_lux)
+		lux = self.ltr559_lux.getValue()
 
-		pressure = self.smooth_value_generic(self.bme280_raw_pressure_window, SAMPLE_WINDOW_LEN, thp[2])
+		# Write current smoothed data to JSON values
+		self.currentValues.proximity, self.currentValues.lux = proximity, lux
 
 		# MICS6814
-		gas_data = MICS6814.read_all()
+		gas = MICS6814.read_all()
 
-		reducing = self.smooth_value_generic(self.mics6814_raw_reducing_window, SAMPLE_WINDOW_LEN, gas_data.reducing)
+		self.mics6814_oxidising.addValue(gas.oxidising)
+		oxidising = self.mics6814_oxidising.getValue()
 
-		oxidising = self.smooth_value_generic(self.mics6814_raw_oxidising_window, SAMPLE_WINDOW_LEN, gas_data.oxidising)
+		self.mics6814_reducing.addValue(gas.reducing)
+		reducing = self.mics6814_reducing.getValue()
 
-		nh3 = self.smooth_value_generic(self.mics6814_raw_nh3_window, SAMPLE_WINDOW_LEN, gas_data.nh3)
+		self.mics6814_nh3.addValue(gas.nh3)
+		nh3 = self.mics6814_nh3.getValue()
 
+		# Write current smoothed data to JSON values
+		self.currentValues.reducing, self.currentValues.oxidising, self.currentValues.nh3 = reducing, oxidising, nh3
 
 		# Send the values to the display
 		self.display.updateValues(proximity, lux, temperature, humidity, pressure, reducing, oxidising, nh3)
-		# Store our values (to turn into json later)
-		self.currentValues.proximity, self.currentValues.lux, self.currentValues.temperature, self.currentValues.humidity, self.currentValues.pressure, self.currentValues.reducing, self.currentValues.oxidising, self.currentValues.nh3 = proximity, lux, temperature, humidity, pressure, reducing, oxidising, nh3
 
 		# update the display - note raw proximity value needed here
 		self.display.draw(r_proximity)
-		# return the sensor values when called (for debugging)
-		return proximity, lux, temperature, humidity, pressure, reducing, oxidising, nh3
 
 	def getJSONValues(self):
-		# Return values formated as json
+		# Return values formatted as JSON
 		return self.currentValues.toJSON()
